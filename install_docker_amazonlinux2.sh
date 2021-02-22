ecf5d841051fffaca7e5d7e8b5d5e6b1284b5703
Geert van Geest <geert.vangeest@sib.swiss>
2021-02-22 12:53:32 +0100

    added docker installation commands

---
+++ install_docker_amazonlinux2.sh
@@ -0,0 +1,4 @@
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
--- run_notebooks.sh
+++ run_notebooks.sh
@@ -3,35 +3,60 @@
 USERLIST=$1
 MASTERPW=$2
 
-# create volume that will be populated with read-only data
+# create general volume that will be populated with read-only data
 docker volume create data
+# create general volume for group work
+docker volume create group
 
+# remove carriage returns and spaces and pipe to while loop
 cat $USERLIST | tr -d '\015\040' | while read port user password
 do
-  echo $port
-  echo $user
-  echo $password
-
+  # generate hash from password
   HASH=`./generate_pw_hash.py -p $password`
 
+  # create user volume. If the container crashes the volume keeps existing.
   docker volume create $user
 
-  docker run -d \
-  --mount source=$user,target=/home/jovyan \
-  --mount source=data,target=/home/jovyan/data,readonly \
+  # generate container for each user
+  # mount user volume, data volume (general, read only) and group work volume (general, read and write)
+  # the permissions of general volumes (data and group work) need to be changed with the master container (this can probably be coded as well)
+  # the --user, -e and -w commands are needed to change the user
+  docker run \
+  -d \
+  --rm \
+  --name $user \
+  --mount source=$user,target=/home/$user \
+  --mount source=data,target=/data,readonly \
+  --mount source=group,target=/group_work \
+  --user root \
+  -e NB_USER=$user \
+  -w /home/$user \
+  -e CHOWN_HOME=yes \
   -p $port:8888 \
   jupyter/base-notebook \
   start-notebook.sh \
   --NotebookApp.password=$HASH
 done
 
-# generate has for master password
+# generate hash for master password
 HASH=`./generate_pw_hash.py -p $MASTERPW`
 
-# generate master container 
-docker run -d \
+# generate admin container
+# the option -e GRANT_SUDO=yes is required to e.g. change permissions in the root directory
+docker run \
+--rm \
+-d \
+--name admin \
 --mount source=data,target=/home/jovyan/data \
+--mount source=group,target=/home/jovyan/group_work \
+--user root \
+-e GRANT_SUDO=yes \
 -p 10000:8888 \
 jupyter/base-notebook \
 start-notebook.sh \
 --NotebookApp.password=$HASH
+
+# stop all containers:
+# docker stop $(docker ps -aq)
+# remove all volumes:
+# docker volume rm $(docker volume ls -q)
