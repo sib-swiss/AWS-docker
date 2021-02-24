73a297ce676579c13a61a913ceee17712f1ef9c9
Geert van Geest <geert.vangeest@sib.swiss>
2021-02-24 08:42:26 +0100

    jupyterlab instead of notebook

--- required_installations_ubuntu.sh
+++ required_installations_ubuntu.sh
@@ -4,6 +4,7 @@ curl https://get.docker.com | sh
 sudo usermod -a -G docker ubuntu
 sudo service docker start
 
+# only for jupyter notebooks:
 sudo apt-get update
 sudo apt-get install -y python3-pip
 pip3 install notebook
