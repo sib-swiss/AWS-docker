b10b55572e5c0ddff46d322c1712a903e645b3b6
Geert van Geest <geert.vangeest@sib.swiss>
2021-02-25 11:31:33 +0100

    notebook installation not required

--- required_installations_ubuntu.sh
+++ required_installations_ubuntu.sh
@@ -7,7 +7,6 @@ sudo service docker start
 # only for jupyter notebooks:
 sudo apt-get update
 sudo apt-get install -y python3-pip
-pip3 install notebook # not needed anymore?
 pip3 install jupyter_server
 
 git clone https://github.com/GeertvanGeest/AWS-docker.git
