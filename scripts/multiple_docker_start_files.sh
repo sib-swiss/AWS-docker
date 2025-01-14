afef519213f562eda06f85f47a1426747f96f329
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2025-01-14 13:47:28 +0100

    additional options rstudio contianer

--- scripts/multiple_docker_start_files.sh
+++ scripts/multiple_docker_start_files.sh
@@ -1,5 +1,5 @@
 
-for p in $(seq 1 6)
+for p in $(seq 0 6)
 do
     sed "s/^10/1$p/g" input_docker_start.txt > input_docker_start_practical_$p.txt
 done
--- scripts/start_multiple_container_groups.sh
+++ scripts/start_multiple_container_groups.sh
@@ -1,7 +1,7 @@
 
 p=1
 ./run_jupyter_notebooks \
-    -i geertvangeest/elixir-sco-spatial-omics:practical_$p \
+    -i sibswiss/elixir-sco-spatial-omics:practical_$p \
     -u credentials/input_docker_start_practical_$p.txt \
     -p test123 \
     -n practical_$p \
@@ -10,9 +10,17 @@ p=1
 for p in 3 6
 do 
     ./run_jupyter_notebooks \
-        -i geertvangeest/elixir-sco-spatial-omics:practical_$p \
+        -i sibswiss/elixir-sco-spatial-omics:practical_$p \
         -u credentials/input_docker_start_practical_$p.txt \
         -p test123 \
         -n practical_$p \
         -a no
 done
+
+p=5
+./run_rstudio_server \
+    -i sibswiss/elixir-sco-spatial-omics:practical_$p \
+    -u credentials/input_docker_start_practical_$p.txt \
+    -p test123 \
+    -n practical_$p \
+    -a no
